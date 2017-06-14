b4a4c81fcc469b32db0ca59f2da68e5ea952665a
Jon Hallett <jon.hallett@bristol.ac.uk>
2017-06-14 16:39:04 +0100

    Add option to redirect

--- manifests/project/apache.pp
+++ manifests/project/apache.pp
@@ -146,6 +146,7 @@ define projects::project::apache::vhost (
   $altnames = [],
   $ip = undef,
   $cert_name = $vhost_name,
+  $redirect_dest,
 ) {
 
   if ($ip) {
@@ -181,31 +182,61 @@ define projects::project::apache::vhost (
     $full_docroot = $docroot
   }
 
-  ::apache::vhost { $title:
-    servername          => $vhost_name,
-    port                => $port,
-    ssl                 => $ssl,
-    docroot             => $full_docroot,
-    logroot             => "${::projects::basedir}/${projectname}/var/log/httpd",
-    use_optional_includes => "true",
-    additional_includes =>
+  if $redirect_dest {
+    ::apache::vhost { $title:
+      servername            => $vhost_name,
+      port                  => $port,
+      ssl                   => $ssl,
+      redirect_dest         => $redirect_dest,
+      logroot               => "${::projects::basedir}/${projectname}/var/log/httpd",
+      use_optional_includes => "true",
+      additional_includes   => 
       ["${::projects::basedir}/${projectname}/etc/apache/conf.d/*.conf",
       "${::projects::basedir}/${projectname}/etc/apache/conf.d/${title}/*.conf"],
-    ssl_cert            =>
+      ssl_cert              => 
       "${::projects::basedir}/${projectname}/etc/ssl/certs/${cert_name}.crt",
-    ssl_chain           =>
+      ssl_chain             => 
       "${::projects::basedir}/${projectname}/etc/ssl/certs/${cert_name}.crt",
-    ssl_key             =>
+      ssl_key               => 
       "${::projects::basedir}/${projectname}/etc/ssl/private/${cert_name}.key",
-    serveraliases       => $altnames,
-    access_log_env_var  => "!forwarded",
-    custom_fragment     => "LogFormat \"%{X-Forwarded-For}i %l %u %t \\\"%r\\\" %s %b \\\"%{Referer}i\\\" \\\"%{User-Agent}i\\\"\" proxy
-SetEnvIf X-Forwarded-For \"^.*\\..*\\..*\\..*\" forwarded
-CustomLog \"${::projects::basedir}/${projectname}/var/log/httpd/${title}_access.log\" proxy env=forwarded",
-    ip                  => $ip,
-    ip_based            => $ip_based,
-    add_listen          => false,
-    headers             => 'Set Strict-Transport-Security "max-age=63072000; includeSubdomains;"',
+      serveraliases         => $altnames,
+      access_log_env_var    => "!forwarded",
+      custom_fragment       => "LogFormat \"%{X-Forwarded-For}i %l %u %t \\\"%r\\\" %s %b \\\"%{Referer}i\\\" \\\"%{User-Agent}i\\\"\" proxy
+      SetEnvIf X-Forwarded-For \"^.*\\..*\\..*\\..*\" forwarded
+      CustomLog \"${::projects::basedir}/${projectname}/var/log/httpd/${title}_access.log\" proxy env=forwarded",
+      ip                    => $ip,
+      ip_based              => $ip_based,
+      add_listen            => false,
+      headers               => 'Set Strict-Transport-Security "max-age=63072000; includeSubdomains;"',
+    }
+  }
+  else {
+    ::apache::vhost { $title:
+      servername          => $vhost_name,
+      port                => $port,
+      ssl                 => $ssl,
+      docroot             => $full_docroot,
+      logroot             => "${::projects::basedir}/${projectname}/var/log/httpd",
+      use_optional_includes => "true",
+      additional_includes =>
+      ["${::projects::basedir}/${projectname}/etc/apache/conf.d/*.conf",
+      "${::projects::basedir}/${projectname}/etc/apache/conf.d/${title}/*.conf"],
+      ssl_cert            =>
+      "${::projects::basedir}/${projectname}/etc/ssl/certs/${cert_name}.crt",
+      ssl_chain           =>
+      "${::projects::basedir}/${projectname}/etc/ssl/certs/${cert_name}.crt",
+      ssl_key             =>
+      "${::projects::basedir}/${projectname}/etc/ssl/private/${cert_name}.key",
+      serveraliases       => $altnames,
+      access_log_env_var  => "!forwarded",
+      custom_fragment     => "LogFormat \"%{X-Forwarded-For}i %l %u %t \\\"%r\\\" %s %b \\\"%{Referer}i\\\" \\\"%{User-Agent}i\\\"\" proxy
+      SetEnvIf X-Forwarded-For \"^.*\\..*\\..*\\..*\" forwarded
+      CustomLog \"${::projects::basedir}/${projectname}/var/log/httpd/${title}_access.log\" proxy env=forwarded",
+      ip                  => $ip,
+      ip_based            => $ip_based,
+      add_listen          => false,
+      headers             => 'Set Strict-Transport-Security "max-age=63072000; includeSubdomains;"',
+    }
   }
 
   if !defined(Apache::Listen["$port"]) {
